1440b42f04c0c2365ee03e0b2e077e0c37497701
Sudeepha <your-email@example.com>
2026-02-07 11:37:52 +0530

    updated api

--- src/api/petApi.js
+++ src/api/petApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/pets';
+const API_BASE_URL = 'https://backend-pet-edhd.onrender.com';
 
 const petApi = {
   getAllPets: async () => {
